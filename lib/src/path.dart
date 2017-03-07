f0ec16977c0faebaa3e4c1ae092383f076a7ed04
Wilson, Marvin (jw9615) <jw9615@us.att.com>
2017-03-07 09:00:47 -0500

    added path and path unit tests

---
+++ lib/src/path.dart
@@ -0,0 +1,43 @@
+abstract class Path {
+  static Path Arcanum = new _Arcanum();
+  static Path Augury = new _Augury();
+  static Path Demonology = new _Demonology();
+  static Path Elementalism = new _Elementalism();
+  static Path Mesmerism = new _Mesmerism();
+  static Path Necromancy = new _Necromancy();
+
+  static List<Path> _list = [Arcanum, Augury, Demonology, Elementalism, Mesmerism, Necromancy];
+
+  final String name;
+  final String aspect;
+
+  Path(this.name, this.aspect);
+
+  factory Path.fromString(String name) {
+    return _list.where((Path e) => e.name == name).first;
+  }
+}
+
+class _Arcanum extends Path {
+  _Arcanum() : super("Arcanum", "Spells, mana, and the creations of the magical arts.");
+}
+
+class _Augury extends Path {
+  _Augury() : super("Augury", "The past, the future, fate, and chance.");
+}
+
+class _Demonology extends Path {
+  _Demonology() : super("Demonology", "Demons and angels; also traveling to and from Hell (but not Heaven!).");
+}
+
+class _Elementalism extends Path {
+  _Elementalism() : super("Elementalism", "Air, earth, fire, water, and wood -- as well as void/sound/ether, if used in the campaign.");
+}
+
+class _Mesmerism extends Path {
+  _Mesmerism() : super("Mesmerism", "The minds of sapient (IQ6+) beings.");
+}
+
+class _Necromancy extends Path {
+  _Necromancy() : super("Necromancy", "The dead and undead, as well as shadows and darkness.");
+}
---
+++ test/path_test.dart
@@ -0,0 +1,101 @@
+import "package:test/test.dart";
+import "package:gurps_incantation_magic_model/incantation_magic.dart";
+
+void main() {
+  group("Arcanum:", () {
+    Path arcanum = new Path.fromString("Arcanum");
+
+    test("can create from String", () {
+      expect(arcanum, isNotNull);
+    });
+
+    test("has name", () {
+      expect(arcanum.name, equals("Arcanum"));
+    });
+
+    test("has aspects", () {
+      expect(arcanum.aspect, equals("Spells, mana, and the creations of the magical arts."));
+    });
+  });
+
+  group("Augury:", () {
+    Path augury = new Path.fromString("Augury");
+
+    test("can create from String", () {
+      expect(augury, isNotNull);
+    });
+
+    test("has name", () {
+      expect(augury.name, equals("Augury"));
+    });
+
+    test("has aspects", () {
+      expect(augury.aspect, equals("The past, the future, fate, and chance."));
+    });
+  });
+
+  group("Demonology:", () {
+    Path demonology = new Path.fromString("Demonology");
+
+    test("can create from String", () {
+      expect(demonology, isNotNull);
+    });
+
+    test("has name", () {
+      expect(demonology.name, equals("Demonology"));
+    });
+
+    test("has aspects", () {
+      expect(demonology.aspect, equals("Demons and angels; also traveling to and from Hell (but not Heaven!)."));
+    });
+  });
+
+  group("Elementalism:", () {
+    Path elementalism = new Path.fromString("Elementalism");
+
+    test("can create from String", () {
+      expect(elementalism, isNotNull);
+    });
+
+    test("has name", () {
+      expect(elementalism.name, equals("Elementalism"));
+    });
+
+    test("has aspects", () {
+      expect(elementalism.aspect,
+          equals("Air, earth, fire, water, and wood -- as well as void/sound/ether, if used in the campaign."));
+    });
+  });
+
+  group("Mesmerism:", () {
+    Path mesmerism = new Path.fromString("Mesmerism");
+
+    test("can create from String", () {
+      expect(mesmerism, isNotNull);
+    });
+
+    test("has name", () {
+      expect(mesmerism.name, equals("Mesmerism"));
+    });
+
+    test("has aspects", () {
+      expect(mesmerism.aspect, equals("The minds of sapient (IQ6+) beings."));
+    });
+  });
+
+  group("Necromancy:", () {
+    Path necromancy = new Path.fromString("Necromancy");
+
+    test("can create from String", () {
+      expect(necromancy, isNotNull);
+    });
+
+    test("has name", () {
+      expect(necromancy.name, equals("Necromancy"));
+    });
+
+    test("has aspects", () {
+      expect(necromancy.aspect, equals("The dead and undead, as well as shadows and darkness."));
+    });
+  });
+}
