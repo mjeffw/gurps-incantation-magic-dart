a499b505e8ad83c8fbacb800a955e70b20aa7d9b
mjeffw <mjeffw@bellsouth.net>
2017-03-12 15:15:35 -0400

    started adding modifier

--- test/modifier_test.dart
+++ test/modifier_test.dart
@@ -2,8 +2,8 @@ import "package:test/test.dart";
 import "package:gurps_incantation_magic_model/incantation_magic.dart";
 
 void main() {
-  group("Afflictions:",(){
-    test("can be created by name",(){
+  group("Afflictions:", () {
+    test("can be created by name", () {
       Modifier m = new Modifier.fromString("Affliction");
     });
   });
