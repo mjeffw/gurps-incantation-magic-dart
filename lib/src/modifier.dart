ea467c0c588af6484ace7974c203dbb1f1d9f634
Wilson, Marvin (jw9615) <jw9615@us.att.com>
2017-03-17 14:00:40 -0400

    start on adding modifier and tests

--- lib/src/modifier.dart
+++ lib/src/modifier.dart
@@ -1,17 +1,75 @@
+/// Describes a modifier to an Incantation Spell.
+///
+/// Modifiers add Damage, Range, Duration, and other features to a spell, and the cost of the spell is adjusted by
+/// the value of the modifiers. Modifiers are identified by their name, and can be inherent (intrisic) or not.
+///
+/// For example, a spell might momentarily open a Gate between dimensions; a Duration modifier can be added to make
+/// the Gate remain for a longer time. The Duration is not inherent or intrinsic in this case.
+///
+/// A spell that adds +2 to the subject's Strength would need a Bestows a Bonus modifier; this effect is inherent to
+/// the spell.
 abstract class Modifier {
-  static Modifier Affliction = new _Affliction();
-
-  static List<Modifier> _list = [Affliction];
-
+  /// the name of this Modifier
   final String name;
+
+  /// number of spell points - dependent on the value and other data
+  int get spellPoints => 0;
+
+  /// is this Modifier instance inherent to the spell?
+  bool _inherent = false;
+
+  /// the current value of this modifier - depends on the modifier, it could represent character points, a time unit,
+  /// distance unit, a percentage modifier, etc.
+  int _value = 0;
 
   Modifier(this.name);
 
-  factory Modifier.fromString(String name) {
-    return _list.where((Modifier e) => e.name == name).first;
+  bool get inherent => _inherent;
+  set inherent(bool i) => _inherent = i;
+
+  int get value => _value;
+  set value(int val) => _value = val;
+
+  /// while SPs depend on value, it is not a one-for-one mapping; a single SP might cover a range of values. this
+  /// returns the min and max values for a given amount of SPs
+  List<int> valueRange(int sp) {
+    return [0, 0];
   }
 }
 
-class _Affliction extends Modifier {
-  _Affliction() : super("Affliction");
+/// Adds the Affliction: Stun (p. B36) effect to a spell.
+///
+/// This effect can be added without additional SP cost.
+class AfflictionStun extends Modifier {
+  AfflictionStun() : super("Affliction (Stun)");
+
+  /// value cannot be set for AfflictionStun
+  @override
+  set value(_) => _;
+}
+
+/// Adds an Affliction (p. B36) effect to a spell.
+///
+/// This adds +1 SP for every +5% it’s worth as an enhancement to Affliction.
+class Affliction extends Modifier {
+  Affliction() : super("Affliction");
+
+  @override
+  set value(int percent) {
+    // negative percent is not allowed
+    if (percent >= 0) {
+      _value = percent;
+    }
+  }
+
+  @override
+  int get spellPoints => (_value / 5.0).ceil();
+
+  @override
+  List<int> valueRange(int sp) {
+    if (sp <= 0) {
+      return [0, 0];
+    }
+    return [((sp-1) * 5) + 1, sp * 5];
+  }
 }
--- test/modifier_test.dart
+++ test/modifier_test.dart
@@ -2,9 +2,67 @@ import "package:test/test.dart";
 import "package:gurps_incantation_magic_model/incantation_magic.dart";
 
 void main() {
-  group("Afflictions:",(){
-    test("can be created by name",(){
-      Modifier m = new Modifier.fromString("Affliction");
+  // A spell effect to stun a foe requires no additional SP.
+  group("Affliction (Stun)", () {
+    Modifier m = new AfflictionStun();
+
+    test("has initial state",(){
+      expect(m.inherent, equals(false));
+      expect(m.value, equals(0));
+      expect(m.name, equals("Affliction (Stun)"));
+      expect(m.spellPoints, equals(0));
+    });
+
+    test("has 0 Spell Points regardless of value", () {
+      m.value = 100;
+      expect(m.spellPoints, equals(0));
+    });
+
+    test("has inherent", () {
+      m.inherent = true;
+      expect(m.inherent, equals(true));
+    });
+
+    test("has valueRange", () {
+      expect(m.valueRange(0), equals([0, 0]));
+      expect(m.valueRange(5), equals([0, 0]));
+      expect(m.valueRange(-5), equals([0, 0]));
+    });
+  });
+
+  group("Afflictions:", () {
+    Modifier m = new Affliction();
+
+    test("has initial state",(){
+      expect(m.inherent, equals(false));
+      expect(m.value, equals(0));
+      expect(m.name, equals("Affliction"));
+      expect(m.spellPoints, equals(0));
+    });
+
+    test("has inherent", () {
+      m.inherent = true;
+      expect(m.inherent, equals(true));
+    });
+
+    test("+1 SP for every +5% it’s worth as an enhancement to Affliction", () {
+      m.value = 10;
+      expect(m.value, equals(10));
+      expect(m.spellPoints, equals(2));
+
+      m.value = 9;
+      expect(m.value, equals(9));
+      expect(m.spellPoints, equals(2));
+
+      m.value = 11;
+      expect(m.value, equals(11));
+      expect(m.spellPoints, equals(3));
+    });
+
+    test("each SP adds 1-5% value", () {
+      expect(m.valueRange(0), equals([0, 0]));
+      expect(m.valueRange(5), equals([21, 25]));
+      expect(m.valueRange(-5), equals([0, 0]));
     });
   });
 }
